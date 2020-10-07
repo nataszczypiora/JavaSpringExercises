91104dd4abb571e3b1373b115f5ac9f6117013da
nataszczypiora <iga423@gmail.com>
2020-10-07 12:45:25 +0200

    IL Ended chapter 23

--- spring-demo-one/src/com/excercises/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/excercises/springdemo/HelloSpringApp.java
@@ -1,10 +1,20 @@
 package com.excercises.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//load the spring configuration file (creating the spring container)
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from container
+		BasicCoach theCoach = context.getBean("myCoach", BasicCoach.class);
+		
+		//caling method from bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//closing the context
+		context.close();
 	}
-
 }
